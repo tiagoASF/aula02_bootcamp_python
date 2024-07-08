ca8c062895869850c7f27a2506e1cd996ef91135
Tiago Fialho <tiago.chem@gmail.com>
2024-07-08 09:53:59 -0300

    Exercicio 13 resolvido

--- exercicios.py
+++ exercicios.py
@@ -1,13 +1,4 @@
 
-
-
-
-
-# #### Strings (`str`)
-
-
-
-# 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
@@ -143,5 +134,10 @@ import math
 
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 
-nome_usuario = input("Informe o nome do usuário: ")
-print(nome_usuario.lower())
+# nome_usuario = input("Informe o nome do usuário: ")
+# print(nome_usuario.lower())
+
+# 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+
+frase = input("Insira uma frase com espaço no início e fim: ")
+print(frase.strip())
